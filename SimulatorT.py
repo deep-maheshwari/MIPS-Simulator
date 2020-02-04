84d8bf9168dbdc9b1c7f5ebbcf3cb7f4bcd09672
Tapish Kumar Ojha <tapishojha2000@gmail.com>
2020-02-04 11:12:47 +0530

    Anything else?

--- SimulatorT.py
+++ SimulatorT.py
@@ -1,7 +1,16 @@
 def parse():
-    text = input().split(" ")
+    text = "add $s1, $s2, $s3"
 
-    for st in text:
+    result = [x.split() for x in text.split(",")]
+    for st in result:
         print(st)
 
+    
+
+    if(result[0][0] == "add"):
+        r1 = add(r2, r3)
+
 parse()
+
+def add(r2, r3):
+    return r2 + r3
