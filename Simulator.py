25ad80757a3011093a578f568c64bff551b67146
unknown <CS18B008@iittp.ac.in>
2020-02-04 11:41:52 +0530

    Third commit

--- Simulator.py
+++ Simulator.py
@@ -1,5 +1,26 @@
 # Simulator for phase 1
-<
-def parse():
-    text = input().split(" ").split(",")
+instructions = ["add","sub","lw","sw","bne"]
+registers = ['s1','s2','s3','s4','s5','s6','s7']
+def fileHandler(filename):
+    file = open(filename,'r')
+    for line in file.readlines():
+        print(line)
+    
+def parse(text):
+    raw = []
+
+    for st in text:
+        raw.append(st.split(","))
+
+    parsed = []
+
+    for item in raw:
+        for st in item:
+            if(st):
+                parsed.append(st)
+
+    print(parsed)
+
+fileHandler("C:/Users/Admin/Desktop/programming/Assembly/question6.s")
+    
     
