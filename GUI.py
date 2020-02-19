7d7c0603c742c88836ab4601f24383af97288a8f
tapish2000 <tapishojha2000@gmail.com>
2020-02-19 13:47:19 +0530

    19/02/2020

--- GUI.py
+++ GUI.py
@@ -1,4 +1,13 @@
 from tkinter import *
+from tkinter import filedialog
+
+def fileHandler(filename):
+
+    file = open(filename,'r')
+    result = []
+    for line in file.readlines():
+        result.append(line)
+    return result
 
 def time_pass():
     print('It works for now!!!')
@@ -8,8 +17,14 @@ app = Tk()
 app.title('DTSpim')
 app.geometry('1000x800')
 
+def openfile():
+    app.filename = filedialog.askopenfilename(initialdir = '/CO', title = 'Select a File', filetypes = (('asm files', '*.asm'), ('s files', '*.s')))
+    # global app.filename
+
+# def 
+
 #Buttons
-load_btn = Button(app, text = 'Load File', width = 18, command = time_pass)
+load_btn = Button(app, text = 'Load File', width = 18, command = openfile)
 load_btn.grid(row = 0, column = 0, pady = 20)
 #Buttons
 run_btn = Button(app, text = 'Run File', width = 18, command = time_pass)
